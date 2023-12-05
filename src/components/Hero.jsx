336de4989fa701dc3bd26cae87eaaa5b96e54206
Mitzi17 <m_estrada17@live.com>
2023-12-04 21:16:19 -0800

    change heading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop frontend web applications{" "}
-            <br className="sm:block hidden" /> using React and Javascript.
+            <br className="sm:block hidden" /> using JavaScript and React.
           </p>
         </div>
       </div>
