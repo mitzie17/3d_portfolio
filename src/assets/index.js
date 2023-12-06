0fa21730bacb0b4d11c2813ec1515875e86a879a
Mitzi17 <m_estrada17@live.com>
2023-12-05 21:58:27 -0800

    import and export mylogo and logo of companies I work at

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import mylogo from "./mylogo.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -21,10 +22,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import csuf from "./company/csuf.png";
+import daiso from "./company/daiso.png";
+import promineo from "./company/promineo.png";
+import zara from "./company/zara.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -32,6 +33,7 @@ import tripguide from "./tripguide.png";
 
 export {
   logo,
+  mylogo,
   backend,
   creator,
   mobile,
@@ -52,10 +54,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  csuf,
+  daiso,
+  promineo,
+  zara,
   carrent,
   jobit,
   tripguide,
